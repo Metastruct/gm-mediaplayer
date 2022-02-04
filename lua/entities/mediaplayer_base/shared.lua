bb017d786039f29ff4e8aab3af8a1fdbe5aad794
Python1320 <python1320+gh@gmail.com>
2022-02-05 01:51:35 +0200

    Delay removal in case the entity is not actually removed

--- lua/entities/mediaplayer_base/shared.lua
+++ lua/entities/mediaplayer_base/shared.lua
@@ -45,7 +45,14 @@ end
 function ENT:OnRemove()
 	local mp = self:GetMediaPlayer()
 	if mp then
-		mp:Remove()
+		if SERVER then 
+			mp:Remove()
+		else
+			timer.Simple(5,function()
+				if self:IsValid() then return end
+				mp:Remove()
+			end)
+		end
 	end
 end
 
